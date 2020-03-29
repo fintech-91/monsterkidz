5d0e700abedb0e648ee20e7dd6bde1bebba52ec9
fintech-91 <fintech91@gmail.com>
2020-03-29 18:52:06 +0530

    Add all files

---
+++ precache-manifest.c50fcf298c47ce158283c4ef20a5244d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436d51a585cc4942c692ffa9e311fdcf",
+    "url": "/monsterkidz/index.html"
+  },
+  {
+    "revision": "da72de4e3c245b03be11",
+    "url": "/monsterkidz/static/css/main.7dbde842.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsterkidz/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterkidz/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da72de4e3c245b03be11",
+    "url": "/monsterkidz/static/js/main.6ebf5e72.chunk.js"
+  },
+  {
+    "revision": "5e89f9bcbec2099e3161",
+    "url": "/monsterkidz/static/js/runtime-main.0f1257f0.js"
+  }
+]);
